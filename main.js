ba39fd1c20d65ac5e8dedc3600a67fcf165a7ece
Bader Alsabah <bhyss@hypertext.com.kw>
2021-04-19 23:18:18 +0500

    implemented mutate & tested it

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // Returns a random DNA base
 const returnRandBase = () => {
-  const dnaBases = ['A', 'T', 'C', 'G'];
+  const dnaBases = ["A", "T", "C", "G"];
   return dnaBases[Math.floor(Math.random() * 4)];
 };
 
@@ -13,10 +13,26 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+const pAequorFactory = (specimenNum, dna) => {
+  return {
+    specimenNum,
+    dna,
+    mutate() {
+      let random = Math.floor(Math.random() * this.dna.length);
+      let originalBase = this.dna[random];
+      let newBase = this.dna[random];
+      while (originalBase === newBase) {
+        newBase = returnRandBase();
+      }
+      this.dna[random] = newBase;
+    },
+  };
+};
 
+// testing first implementation of pAequorFactor
+// console.log(pAequorFactory(100, ["A", "C"]));
 
-
-
-
-
-
+// testing #4
+let specimen = pAequorFactory(100, ["A", "T", "C"]);
+specimen.mutate();
+console.log(specimen);
